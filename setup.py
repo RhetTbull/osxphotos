55a6b5bf1cfb8078c4e24e7450d83c377d5de09a
Rhet Turnbull <rturnbull@gmail.com>
2019-11-30 11:03:07 -0800

    version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="osxphotos",
-    version="0.14.7",
+    version="0.14.8",
     description="Manipulate (read-only) Apple's Photos app library on Mac OS X",
     long_description=long_description,
     long_description_content_type="text/markdown",
