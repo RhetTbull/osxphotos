6ab1511b4f0483ee160f0f0bc93d710779d3c05c
Rhet Turnbull <rturnbull+git@gmail.com>
2020-03-21 11:15:39 -0700

    Added pycodestyle needed by bpylist2

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
+        "pycodestyle==2.3.1",  # needed by bpylist==2.0.3
         "pyobjc>=6.0.1",
         "Click>=7",
         "PyYAML>=5.1.2",
