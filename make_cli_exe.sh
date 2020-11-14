2d9429c8eefabe6233fc580f65511c48ee6c01e5
Rhet Turnbull <rturnbull+git@gmail.com>
2020-11-14 14:18:47 -0800

    Fixed missing data file for photoscript

--- make_cli_exe.sh
+++ make_cli_exe.sh
@@ -5,4 +5,4 @@
 # If you need to install pyinstaller:
 # python3 -m pip install --upgrade pyinstaller
 
-pyinstaller --onefile --hidden-import="pkg_resources.py2_warn" --name osxphotos --add-data osxphotos/templates/xmp_sidecar.mako:osxphotos/templates cli.py
+pyinstaller osxphotos.spec
