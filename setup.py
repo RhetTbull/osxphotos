75516d91e959019759337fd254d601283cc04af2
Rhet Turnbull <rturnbull+git@gmail.com>
2021-01-21 06:21:11 -0800

    Added python versions to setup.py

--- setup.py
+++ setup.py
@@ -68,6 +68,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: MacOS :: MacOS X",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
