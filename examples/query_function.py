49adff1f3b6720641afc254b0ca5fe0e2fc0a524
Rhet Turnbull <rturnbull@gmail.com>
2021-06-20 18:11:41 -0700

    Updated example [skip ci]

--- examples/query_function.py
+++ examples/query_function.py
@@ -5,7 +5,7 @@ from typing import List
 from osxphotos import PhotoInfo
 
 
-# call this with --query-function:examples/query_function.py::best_selfies
+# call this with --query-function examples/query_function.py::best_selfies
 def best_selfies(photos: List[PhotoInfo]) -> List[PhotoInfo]:
     """your query function should take a list of PhotoInfo objects and return a list of PhotoInfo objects (or empty list)"""
     # this example finds your best selfie for every year
