0e9b9d625190b94c1dd68276e3b0e5367002d87c
Rhet Turnbull <rturnbull@gmail.com>
2021-10-15 05:45:17 -0700

    Updated docs [skip ci]

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.42.93',
+    VERSION: '0.42.94',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
