7521c652a96393a832676377324c5cc4abafffb2
Rhet Turnbull <rturnbull@gmail.com>
2022-09-10 21:31:26 -0700

    Fixed tests

--- tests/test_sqlite_utils.py
+++ tests/test_sqlite_utils.py
@@ -6,15 +6,7 @@ import sqlite3
 from osxphotos.sqlite_utils import sqlite_open_ro, sqlite_db_is_locked
 
 
-DB_LOCKED_10_12 = "./tests/Test-Lock-10_12.photoslibrary/database/photos.db"
-DB_LOCKED_10_15 = "./tests/Test-Lock-10_15_1.photoslibrary/database/Photos.sqlite"
 DB_UNLOCKED_10_15 = "./tests/Test-10.15.1.photoslibrary/database/photos.db"
-
-
-def test_db_is_locked_locked():
-
-    assert sqlite_db_is_locked(DB_LOCKED_10_12)
-    assert sqlite_db_is_locked(DB_LOCKED_10_15)
 
 
 def test_db_is_locked_unlocked():
