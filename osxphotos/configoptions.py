c67c69425fc7e919866beefc7d4dcd7b12ebf143
Christian Clauss <cclauss@me.com>
2023-04-03 08:52:52 +0200

    Lint Python twice: 1. Whole repo, 2. Exclude tests/
    Both runs take less than a second so...
    `line-length=366` is more reasonable`
    ```
        - run: pip install --user ruff  # Lint Python twice: 1. Whole repo, 2. Exclude tests/
        - run: ruff --format=github --line-length=7228 --target-version=py39
                    --ignore=E402,E712,E721,E722,E741,F401,F403,F405,F541,F601,F811,F822,F841 .
        - run: ruff --format=github --line-length=366 --target-version=py39
                    --exclude=tests/ --ignore=E402,E712,E722,E741,F401,F403,F405,F541,F822,F841 .
    ```

--- osxphotos/configoptions.py
+++ osxphotos/configoptions.py
@@ -50,7 +50,7 @@ class ConfigOptions:
             try:
                 arg = args[attr]
                 # don't test 'not arg'; need to handle empty strings as valid values
-                if arg is None or arg == False:
+                if arg in (False, None):
                     if type(self._attrs[attr]) == tuple:
                         setattr(self, attr, ())
                     else:
