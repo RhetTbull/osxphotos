cd4d253ffb92fde3c98b63f5e3ca5a0cdb44bfc3
Rhet Turnbull <rturnbull@gmail.com>
2023-06-19 11:15:16 -0700

    Added tests for unicode utilities

--- osxphotos/unicode.py
+++ osxphotos/unicode.py
@@ -9,10 +9,10 @@ from typing import Literal, TypeVar, Union
 from osxphotos.platform import is_macos
 
 # Unicode format to use for comparing strings
-UNICODE_FORMAT = "NFC"
+UNICODE_FORM = "NFC"
 
 # Unicode format to use for filesystem paths
-UNICODE_FS_FORMAT = "NFD" if is_macos else "NFC"
+UNICODE_FS_FORM = "NFD" if is_macos else "NFC"
 
 PathType = TypeVar("PathType", bound=Union[str, pathlib.Path])
 
@@ -21,65 +21,66 @@ UnicodeDataType = TypeVar(
 )
 
 __all__ = [
-    "get_unicode_format",
-    "set_unicode_format",
-    "get_unicode_fs_format",
-    "set_unicode_fs_format",
+    "get_unicode_form",
+    "set_unicode_form",
+    "get_unicode_fs_form",
+    "set_unicode_fs_form",
     "normalize_fs_path",
     "normalize_unicode",
 ]
 
 
-def get_unicode_format() -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
+def get_unicode_form() -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
     """Return the global unicode format"""
-    global UNICODE_FORMAT
-    return UNICODE_FORMAT
+    global UNICODE_FORM
+    return UNICODE_FORM
 
 
-def set_unicode_format(format: Literal["NFC", "NFKC", "NFD", "NFKD"]) -> None:
+def set_unicode_form(format: Literal["NFC", "NFKC", "NFD", "NFKD"]) -> None:
     """Set the global unicode format"""
 
     if format not in ["NFC", "NFKC", "NFD", "NFKD"]:
         raise ValueError(f"Invalid unicode format: {format}")
 
-    global UNICODE_FORMAT
-    UNICODE_FORMAT = format
+    global UNICODE_FORM
+    UNICODE_FORM = format
 
 
-def get_unicode_fs_format() -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
+def get_unicode_fs_form() -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
     """Return the global unicode filesystem format"""
-    global UNICODE_FS_FORMAT
-    return UNICODE_FS_FORMAT
+    global UNICODE_FS_FORM
+    return UNICODE_FS_FORM
 
 
-def set_unicode_fs_format(format: str) -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
+def set_unicode_fs_form(format: str) -> Literal["NFC", "NFKC", "NFD", "NFKD"]:
     """Set the global unicode filesystem format"""
 
     if format not in ["NFC", "NFKC", "NFD", "NFKD"]:
         raise ValueError(f"Invalid unicode format: {format}")
 
-    global UNICODE_FS_FORMAT
-    UNICODE_FS_FORMAT = format
+    global UNICODE_FS_FORM
+    UNICODE_FS_FORM = format
 
 
 def normalize_fs_path(path: PathType) -> PathType:
     """Normalize filesystem paths with unicode in them"""
-    global UNICODE_FS_FORMAT
+    form = get_unicode_fs_form()
     if isinstance(path, pathlib.Path):
-        return pathlib.Path(unicodedata.normalize(UNICODE_FS_FORMAT, str(path)))
+        return pathlib.Path(unicodedata.normalize(form, str(path)))
     else:
-        return unicodedata.normalize(UNICODE_FS_FORMAT, path)
+        return unicodedata.normalize(form, path)
 
 
 def normalize_unicode(value: UnicodeDataType) -> UnicodeDataType:
     """normalize unicode data"""
+    form = get_unicode_form()
     if value is None:
         return None
     if isinstance(value, tuple):
-        return tuple(unicodedata.normalize(UNICODE_FORMAT, v) for v in value)
+        return tuple(unicodedata.normalize(form, v) for v in value)
     elif isinstance(value, list):
-        return [unicodedata.normalize(UNICODE_FORMAT, v) for v in value]
+        return [unicodedata.normalize(form, v) for v in value]
     elif isinstance(value, str):
-        return unicodedata.normalize(UNICODE_FORMAT, value)
+        return unicodedata.normalize(form, value)
     else:
         return value
