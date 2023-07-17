ff70981fbb39d7ab8474a6a486bd2c86b8fead3f
Rhet Turnbull <rturnbull@gmail.com>
2023-07-16 19:01:52 -0700

    Release v0.60.9 (#1120)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.60.8',
+    VERSION: '0.60.9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
