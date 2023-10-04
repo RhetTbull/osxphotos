cbff765f1cdd9b73d601e8991ad7f5abe8130ca9
Rhet Turnbull <rturnbull@gmail.com>
2023-10-03 22:27:20 -0700

    Fixed fingerprint test for linux

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -3,8 +3,12 @@
 import pytest
 
 from osxphotos import PhotosDB
+from osxphotos.platform import is_macos
 
-fingerprint = pytest.importorskip("osxphotos.fingerprint", reason="macOS only")
+if not is_macos:
+    pytest.skip("skipping fingerprint tests, macOS only", allow_module_level=True)
+
+from osxphotos.fingerprint import fingerprint
 
 LIBRARY_PATH = "tests/Test-13.0.0.photoslibrary"
 UUID = "6191423D-8DB8-4D4C-92BE-9BBBA308AAC4"
@@ -14,4 +18,4 @@ def test_fingerprint():
     """Test that fingerprint() returns the same as Photos fingerprint"""
     photos = PhotosDB(LIBRARY_PATH).photos(uuid=[UUID])
     photo = photos[0]
-    assert photo.fingerprint == fingerprint.fingerprint(photo.path)
+    assert photo.fingerprint == fingerprint(photo.path)
