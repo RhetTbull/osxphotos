1d63ddcbfb1d4ca307f0d207bcb43241b1105b68
Rhet Turnbull <rturnbull@gmail.com>
2023-12-22 12:36:56 -0800

    Added new_text example

---
+++ examples/new_text.py
@@ -0,0 +1,35 @@
+"""Query any photos since the last run and extract text from them."""
+
+import datetime
+import json
+
+import osxphotos
+from osxphotos.cli import echo, kvstore, query_command
+
+
+@query_command()
+def new_text(photos: list[osxphotos.PhotoInfo], **kwargs):
+    """Query any photos since the last run and extract text from them."""
+    kv = kvstore("osxphotos_new_text")
+    results = []
+    for photo in photos:
+        if photo.uuid in kv:
+            continue
+        if text := photo.search_info.detected_text:
+            data = {
+                "photo": photo.original_filename,
+                "uuid": photo.uuid,
+                "date_added": photo.date_added.isoformat()
+                if photo.date_added
+                else None,
+                "text": text,
+            }
+            results.append(data)
+        kv[photo.uuid] = datetime.datetime.now().isoformat()
+    kv.close()
+    if results:
+        echo(json.dumps(results, indent=4))
+
+
+if __name__ == "__main__":
+    new_text()
