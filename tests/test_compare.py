889640da59256ff907e06d601e84b7844f2facee
Rhet Turnbull <rturnbull@gmail.com>
2024-03-03 13:43:51 -0800

    Added tests for diff and signature functions

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -9,6 +9,7 @@ import json
 import pytest
 
 from osxphotos.compare_libraries import compare_photos_libraries
+from osxphotos.photoinfo import PhotoInfo
 from osxphotos.photosdb import PhotosDB
 
 LIBRARY_A = "tests/Test-13.5.1-compare-1.photoslibrary"
@@ -123,3 +124,34 @@ def test_compare_photos_libraries_bool(diff_a_b):
 def test_compare_photos_libraries_bool(diff_a_a):
     """Test comparison len()"""
     assert not bool(diff_a_a)
+
+
+def test_compare_photos_libraries_diff_function(db_a, db_b):
+    """Test custom comparison diff function"""
+
+    def diff_func(a: PhotoInfo, b: PhotoInfo):
+        # return difference in keywords, if any
+        kw_a = set(a.keywords)
+        kw_b = set(b.keywords)
+        return kw_a.symmetric_difference(kw_b)
+
+    diff = compare_photos_libraries(db_a, db_b, diff_function=diff_func)
+    assert len(diff.in_a_not_b) == 1
+    assert len(diff.in_b_not_a) == 2
+    assert len(diff.in_both_same) == 2
+    assert len(diff.in_both_different) == 1
+
+
+def test_compare_photos_libraries_signature_function(db_a, db_b):
+
+    def signature_func(p: PhotoInfo):
+        sig, _ = p.render_template(
+            "{photo.original_filename|lower}:{photo.original_filesize}"
+        )
+        return sig[0]
+
+    diff = compare_photos_libraries(db_a, db_b, signature_function=signature_func)
+    assert len(diff.in_a_not_b) == 1
+    assert len(diff.in_b_not_a) == 2
+    assert len(diff.in_both_same) == 2
+    assert len(diff.in_both_different) == 1
