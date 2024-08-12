59b4cd236098b062e8ac31befe1ce912171fe5cc
oPromessa <mario@silvapereira.com>
2024-08-12 01:02:47 +0100

    Update conftest.py to run --tests-sync on commpatibility mode OS_VER: 10.16

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,7 +68,7 @@ def get_os_version():
 
 
 OS_VER = get_os_version() if is_macos else [None, None]
-if OS_VER[0] == "10" and OS_VER[1] == "15":
+if OS_VER[0] == "10" and OS_VER[1] in ("15", "16"):
     # Catalina
     TEST_LIBRARY = "tests/Test-10.15.7.photoslibrary"
     TEST_LIBRARY_IMPORT = TEST_LIBRARY
