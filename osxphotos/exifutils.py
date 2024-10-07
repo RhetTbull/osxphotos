36c8332258d19402f96ddfd9175f6a6a77af6a61
MSP <mario@silvapereira.com>
2024-10-07 17:51:31 +0100

    Proposed fiz for #1703 timewarp pull-exif timezone fails to update Photos Timezone info when offset_seconds

--- osxphotos/exifutils.py
+++ osxphotos/exifutils.py
@@ -97,7 +97,7 @@ def get_exif_date_time_offset(
 
     # try to get offset from EXIF:OffsetTimeOriginal
     offset = exif.get("EXIF:OffsetTimeOriginal") or exif.get("OffsetTimeOriginal")
-    if dt and not offset:
+    if dt and offset is None:
         # see if offset set in the dt string
         for pattern in (
             r"\d{4}:\d{2}:\d{2}\s\d{2}:\d{2}:\d{2}([+-]\d{2}:\d{2})",
@@ -119,10 +119,10 @@ def get_exif_date_time_offset(
                 dt = f"{matched.group(1)} 00:00:00"
                 default_time = True
 
-    offset_seconds = exif_offset_to_seconds(offset) if offset else None
+    offset_seconds = exif_offset_to_seconds(offset) if offset is not None else None
 
     if dt:
-        if offset:
+        if offset is not None:
             # drop offset from dt string and add it back on in datetime %z format
             dt = re.sub(r"[+-]\d{2}:\d{2}$", "", dt)
             dt = re.sub(r"\.\d+$", "", dt)
