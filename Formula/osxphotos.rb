ec99870df694f789bda76beb28649c89c27611e6
Rhet Turnbull <rturnbull@gmail.com>
2025-06-16 06:57:19 -0500

    Added workflow for homebrew release (#1879)
    * Added homebrew formula
    
    * Added homebrew formula
    
    * Added workflow to auto-update homebrew formula

---
+++ Formula/osxphotos.rb
@@ -0,0 +1,20 @@
+class Osxphotos < Formula
+  include Language::Python::Virtualenv
+
+  desc "Export photos from Apple Photos app and query the Photos database"
+  homepage "https://github.com/RhetTbull/osxphotos"
+  version "__VERSION__"
+  url "https://files.pythonhosted.org/packages/source/o/osxphotos/osxphotos-#{version}.tar.gz"
+  sha256 "__SHA256__"
+  license "MIT"
+
+  depends_on "python@3.13"
+  depends_on :macos
+
+  def install
+    virtualenv_create(libexec, "python3")
+    system libexec/"bin/python", "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"
+    system libexec/"bin/python", "-m", "pip", "install", buildpath.to_s
+    bin.install_symlink libexec/"bin/osxphotos"
+  end
+end
