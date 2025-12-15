cc541c0ae4c50f50ce1be909ba59a7e97f388354
Rhet Turnbull <rturnbull@gmail.com>
2025-12-14 19:47:54 -0600

    Completions (#2037)
    * Added shell completions to CLI
    
    * Fixed fish completions
    
    * Renamed shell-completion command

---
+++ osxphotos/share/osxphotos-completion.bash
@@ -0,0 +1,29 @@
+_osxphotos_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _OSXPHOTOS_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_osxphotos_completion_setup() {
+    complete -o nosort -F _osxphotos_completion osxphotos
+}
+
+_osxphotos_completion_setup;
+
